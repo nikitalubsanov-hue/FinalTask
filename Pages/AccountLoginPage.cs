0ee82c95aaf3a18db73a379c57264a6106b2d735
Mykyta Lubsanov <nikita.lubsanov@gmail.com>
2026-04-01 23:00:09 +0200

    Added Pages proj, and PageObjects.

---
+++ Pages/AccountLoginPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Pages
+{
+    public class AccountLoginPage(IWebDriver driver) : BasePage(driver)
+    {
+        private readonly By ContinueButton = By.CssSelector("button.btn-orange[title='Continue']");
+
+        public void ClickContinueNewCustomer() => driver.FindElement(ContinueButton).Click();
+    }
+}
---
+++ Pages/MainPage.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+namespace Pages
+{
+    public class MainPage(IWebDriver driver) : BasePage(driver)
+    {
+        private readonly By LoginRegisterLink = By.CssSelector("#customer_menu_top li a");
+        private readonly By SpecialsLink = By.CssSelector("li[data-id='menu_specials'] a");
+        public void GoToLoginRegister() => driver.FindElement(LoginRegisterLink).Click();
+        public void GoToSpecials() => driver.FindElement(SpecialsLink).Click();
+    }
+}
